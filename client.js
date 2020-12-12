864d552c8eb3ae4eb7dfb4341421f7a0cb73571d
Connor Detviler <connor.detviler@gmail.com>
2020-12-11 21:29:10 -0600

    fixed data handling related bug that resulted in the monthly expenditure being a huge number

--- client.js
+++ client.js
@@ -1,33 +1,36 @@
-employees = [
+let employees = [
     {
         firstName: 'Jen',
         lastName: 'Barber',
-        id: 4521,
+        id: '4521',
         title: 'Team Lead',
-        annualSalary: 80000
+        annualSalary: '80000'
     },
     {
         firstName: 'Maurice',
         lastName: 'Moss',
-        id: 8724,
+        id: '8724',
         title: 'Support Team',
-        annualSalary: 58000
+        annualSalary: '58000'
     },
     {
         firstName: 'Roy',
         lastName: 'Smith',
-        id: 9623,
+        id: '9623',
         title: 'Quality Assurance',
-        annualSalary: 48000
+        annualSalary: '48000'
     },
     {
         firstName: 'Harry',
         lastName: 'Jenson',
-        id: 4973,
+        id: '4973',
         title: 'Team Lead',
-        annualSalary: 80000
+        annualSalary: '80000'
     }
 ];
+
+// let yearlySpent = 0;
+// let monthlySpent = 0;
 
 $(readyNow);
 
@@ -37,6 +40,11 @@ function readyNow() {
 
 function documentReady() {
     printAllEmployees(employees);
+
+    $('#add-employee-submit').click(function(e) {
+        e.preventDefault(); //found this solution online. the button was reloading the page. This prevents that.
+        addNewEmployee();
+    });
 }
 
 function displayEmployee(emp) {
@@ -47,23 +55,48 @@ function displayEmployee(emp) {
     $(`#${employeeRowID}`).append(`<td>${emp.lastName}</td>`);
     $(`#${employeeRowID}`).append(`<td>${emp.id}</td>`);
     $(`#${employeeRowID}`).append(`<td>${emp.title}</td>`);
-    $(`#${employeeRowID}`).append(`<td>${emp.annualSalary}</td>`);
+    $(`#${employeeRowID}`).append(`<td>$${emp.annualSalary}</td>`);
     $(`#${employeeRowID}`).append(`<td><button id="delete-${employeeRowID}">Delete</button></td>`);
 }
 
 function printAllEmployees(array) {
+    $('#employee-table').empty();
+
     let yearlySpent = 0;
+    let monthlySpent = 0;
+
     for (let i = 0; i < array.length; i++) {
         displayEmployee(array[i]);
 
-        yearlySpent += array[i].annualSalary;
+        yearlySpent += Number(array[i].annualSalary);
     }
-    // console.log('yearly spent:' + yearlySpent);
-    let monthlySpent = yearlySpent / 12;
+    monthlySpent = yearlySpent / 12;
 
+    $('#spent-monthly').text('');
     $('#spent-monthly').text(Math.round(monthlySpent)); //stretch: round to nearest cent instead
 
     if(monthlySpent > 20000) {
-        $('#monthly-expense-text').addClass('btn-danger');
+        $('#monthly-expense-text').addClass('expensive');
     }
+}
+
+function addNewEmployee() {
+    console.log('adding new employee')
+    let newEmp = {
+        firstName: $('#first-name-in').val(),
+        lastName: $('#last-name-in').val(),
+        id: $('#id-in').val(),
+        title: $('#title-in').val(),
+        annualSalary: $('#salary-in').val()
+    }
+
+    employees.push(newEmp);
+
+    $('#first-name-in').val('');
+    $('#last-name-in').val('');
+    $('#id-in').val('');
+    $('#title-in').val('');
+    $('#salary-in').val('');
+
+    printAllEmployees(employees);
 }
